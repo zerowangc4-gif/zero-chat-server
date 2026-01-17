7b89ecd04145cefa59e52036e85cbe21a20ba179
wangc4 <1172524987@qq.com>
2026-01-18 01:18:27 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,8 @@
 import express from "express";
 import cors from "cors";
-import { mysql, redis } from "@/config"; // 引入你命名的两个大将
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get("/health", async (req, res) => {
-  const isDev = process.env.NODE_ENV === "development";
-  try {
-    await mysql.query("SELECT 1");
-    await redis.ping();
-
-    res.status(200).json({ status: "ok", service: "zero-chat-server" });
-  } catch (err: any) {
-    res.status(500).json({
-      status: "unhealthy",
-      error: isDev ? err.message : "Database Connection Error",
-    });
-  }
-});
 
 export default app;
