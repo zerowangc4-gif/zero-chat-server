15cd3d9b723aab78876c6a1dc5afffaea597fb61
wangc4 <1172524987@qq.com>
2026-01-19 00:58:01 +0800

    再次调试

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", rootRouter);
 
-app.all("*", (req, res) => {
-  res.status(404).json({
-    code: 404,
-    message: `不能发现: ${req.originalUrl}`,
-  });
-});
-
 app.use(errorHandler);
 
 export default app;
