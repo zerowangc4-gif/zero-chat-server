621e04584d4e0d1a30c85f63bfe2ff656d4f88c6
wangc4 <1172524987@qq.com>
2026-02-04 00:07:33 +0800

    Update authController.ts

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -23,7 +23,7 @@ export const login = catchAsync(async (req: Request, res: Response, next: NextFu
 });
 
 export const getNonce = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  console.log("dddddd", req);
+  console.log("dddd", req);
   const { address } = req.body;
   if (!address) {
     throw new AppError(400, "Missing required parameters");
