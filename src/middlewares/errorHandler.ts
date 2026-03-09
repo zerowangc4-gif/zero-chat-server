75e7ba5b48ebb7010598de474df4bfa4f2f944ce
wangc4 <1172524987@qq.com>
2026-03-10 01:17:02 +0800

    重试流程

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -5,7 +5,7 @@ export const errorHandler = (error: AppError, req: Request, res: Response, _next
   const statusCode = error.code || 500;
   const message = error.code ? error.message : "Internal Server Error";
   if (statusCode === 500) {
-    console.error("error originalUrl", error);
+    console.error("error originalUrl", req.originalUrl);
   }
   res.status(statusCode).json({
     success: false,
