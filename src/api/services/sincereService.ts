9d3d3bf5e47687bab743509c5fc03e90829f9015
wangc4 <1172524987@qq.com>
2026-03-10 03:30:53 +0800

    重置文件

--- src/api/services/sincereService.ts
+++
@@ -1,6 +0,0 @@
-import { updateAvatarSeed } from "@/models";
-import { AppError } from "@/types";
-
-export async function handlewechatLogin(address: string, avatarSeed: string): Promise<void> {
-  await updateAvatarSeed(address, avatarSeed);
-}
--- src/routes/sincere.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { wechatLogin } from "@/api";
-
-const router = Router();
-
-router.post("/wechatLogin", wechatLogin);
-
-export default router;
